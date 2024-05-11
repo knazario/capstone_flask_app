c158be34926f3fef5a7712794213b3fab1ba4e09
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-05-11 15:40:52 -0700

    Uploading flask app

---
+++ app.py
@@ -0,0 +1,89 @@
+# Import Dependencies
+from flask import Flask, redirect, url_for, render_template, send_from_directory, request
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+# Establish Falsk App, defining static and template folders for rendering
+app = Flask(__name__, static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+
+# Prediction Function 
+def predict_rating(predict_row):
+    loaded_model = tf.keras.models.load_model('static/resources/model.h5')  # Loading trained model
+    result = loaded_model.predict(predict_row)  # Making a single prediction based on passed data-row and returning probablity result 
+    return result[0][0] 
+
+def pred_accuracy(prediction, target):
+    if prediction == target:
+        return "The model's prediction was correct!"
+    else: 
+        return "The model got it wrong for this wine"
+
+# Root endpoint (Landing/Main page with links to the other endpoints)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# API endpoint to redirect to Tableau Dashboard
+@app.route('/dashboard')
+def dashboard():
+    dashboard = 'https://public.tableau.com/app/profile/viktor.kabelkov/viz/WineAnalysis_17149494454560/WineInfoDaashboard'
+    return redirect(dashboard)
+
+# API endpoint to making prediction page based on X_Test Dataset
+@app.route('/makeprediction')
+def makeprediction():
+    return render_template('Prediction.html')
+
+# API endpoint to redirect to github repo
+@app.route('/repo')
+def repo():
+    return redirect('https://github.com/Zetaorionis/Capstone_Project/tree/main')
+
+# API endpoint for displaying prediction results. Function loads model and makes prediciton
+@app.route('/result', methods = ['POST'])
+def result3():
+    # Loading wine data (for metadata display) & scaled data (for model prediction)
+    wine_df = pd.read_csv('static/resources/clean_wine_data_final.csv')
+    scaled_df = pd.read_csv('static/resources/scaled_data_df.csv')
+
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()    # turns form response into a dictionary
+        wine = int(to_predict_list['number'])   # extracts value entered and sets as integer
+        
+        # Extracts index from scaled_df and pulls metadata for selected wine based on index
+        predict_index = scaled_df.iloc[wine,-2]
+        wine_data = wine_df.iloc[predict_index,:]
+        
+        # extracts row based on integer and excludes index and target (last 2 columns) as numpy array and 
+        #converts to float32 for tensor model     
+        predict_row = np.expand_dims(scaled_df.iloc[wine,:-2], axis=0)  
+        predict_row = np.asarray(predict_row).astype(np.float32)
+       
+        # Calls predict_rating function and passes numpy array of scaled data
+        prediction = predict_rating(predict_row)
+
+        # Sets response text based on prediction probabilty (less than .5 = False/< 90) and adds probability value
+        if prediction < .5:
+            prediction_text = f"We predict this wine is less than 90 points (Probability: {round(float(prediction),2)})"
+            accuracy = pred_accuracy(False,scaled_df.iloc[wine,-1])
+        else: 
+            prediction_text = f"This wine is 90 points or higher! (Probability: {round(float(prediction),2)})"
+            accuracy = pred_accuracy(True,scaled_df.iloc[wine,-1])
+
+        # Create content dictionary based on wine metadata and add a prediction key with prediction text
+        result = wine_data.to_dict()
+        result['prediction'] = prediction_text
+        result['accuracy'] = accuracy
+
+    return render_template('result.html', result = result)  # render results template and pass content dictionary for display 
+
+# API endpoint to Trained Model Landing Page
+@app.route('/makeprediction2')
+def makeprediction2():
+    return render_template('Prediction.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
